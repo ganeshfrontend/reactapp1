f5ab058b613041bed29e0abab7274598787657be
ganeshfrontend <padalaganesh3130@gmail.com>
2023-08-30 10:55:49 +0530

    tododone

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import Counter from './Counter';
-import Todolist from './Todolist';
-import Iplteam from './Iplteam';
 import Todolist1 from './Todolist-1';
 function App() {
   return (
     <div className="tarun">
       <h1>welcome to ganesh world</h1>
-    <Counter start={10} i={10}></Counter>
-    <Counter start={5} i={5}></Counter>
-    <Todolist></Todolist>
-    <Iplteam></Iplteam>
     <Todolist1></Todolist1>
     </div>
   );
--- src/Todolist-1.jsx
+++ src/Todolist-1.jsx
@@ -25,17 +25,28 @@ status:true
 ])
 function done(i){
 var temp=[...todo]
-temp.splice(i,1)
+temp[i].status=!temp[i].status
 setTodo([...temp])
+}
+function add(){
+var input=document.getElementById("input").value
+setTodo([...todo,{title:input}])
 }
 return(
 <div className='tarun'>
 <h1>Todolist1:</h1>
+<input type="text"  id="input"/>
+<button onClick={()=>{add()}}>AddTask</button>
 {
 todo.map((todos,i)=>{
-return <li className='tarun'>{todos.title} {todos.status}
-<button onClick={done(i)}>Done</button>
+return (
+<div className='tarun'>
+<li style={(todos.status===true)?{backgroundColor:"blue"}:{backgroundColor:"purple"}}>{todos.title}
+<button onClick={()=>{done(i)}}>Done</button>
+<button >Undo</button>
 </li>
+</div>
+)
 })
 }
 </div>
