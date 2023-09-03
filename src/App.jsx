256d6eddae04d701348e1cf54ea302c440c494d0
ganeshfrontend <padalaganesh3130@gmail.com>
2023-09-03 20:51:05 +0530

    rerender

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import Refactoring from "./Refactoring";
 function App() {
   return(
 <div className="mybox">
-  <h1 align="center">Refactoring</h1>
-  <Refactoring></Refactoring>
+<h1>Counter:</h1>
+<button>Increment</button>
 </div>
   )
 }
