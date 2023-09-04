306fefd32d6d3c5bd0d49378d6d28b49ef90397e
ganeshfrontend <padalaganesh3130@gmail.com>
2023-09-04 15:00:14 +0530

    accord

--- src/Accordian.jsx
+++ src/Accordian.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 function Accordian(){
 var [d,setD]=React.useState([
 "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
 "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
 "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
 "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
-""
+"Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan"
 ])
 function bu(){
-
+setD([...d])
+alert("hi")
 }
 return(
 <div>
