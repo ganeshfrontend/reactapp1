9f5707414ced07a219692a2e0bb6a7e5dfe5fc77
ganeshfrontend <padalaganesh3130@gmail.com>
2023-09-07 21:02:34 +0530

    vacation

--- src/Vacationplanner.jsx
+++ src/Vacationplanner.jsx
@@ -10,26 +10,30 @@ console.log(response.data)
 setVacation([...response.data])
  
 })},[])
+function int(){
+var temp=[...vacation]
+temp.splice(0,1)
+setVacation(temp)
+}
 return(
-<div className="card mybox" style={{width:"18rem;"}}>
+<div className=" mybox" style={{width:"18rem;"}}>
  {
  vacation.map(function(v){
-return <li className="m-2">
-    <img src={v.image}  className="card-img-top"/><br />
+return <li className="card m-5">
+    <img src={v.image}  className="card-img-top img"/><br />
 <div className="card-body">
 <h5 className="card-title">{v.name} 
 <h6 className="badge bg-primary">${v.price}</h6>
 </h5>
 <p className="card-text">{v.info}</p>
 <div className="d-grid gap-2 col-4 mx-auto">
-<button className="btn btn-outline-danger">Not Interested</button>
+<button className="btn btn-outline-danger" onClick={int}>Not Interested</button>
 </div>
 </div>
 </li>
 })
 }
 </div>
-
 )
 }
 export default Vacationplanner;
